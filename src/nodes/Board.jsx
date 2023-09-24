b1958288c2a1b03c281a776939c377e3f7a3b94c
Sofia Tejerina <sofiatejerina23@gmail.com>
2023-09-23 23:56:42 -0300

    Delete unused things

--- src/nodes/Board.jsx
+++ src/nodes/Board.jsx
@@ -17,9 +17,6 @@ const minimapStyle = {
   height: 120,
 };
 
-const onInit = (reactFlowInstance) =>
-  console.log("flow loaded:", reactFlowInstance);
-
 const Board = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
@@ -28,26 +25,13 @@ const Board = () => {
     []
   );
 
-  // we are using a bit of a shortcut here to adjust the edge type
-  // this could also be done with a custom edge for example
-  const edgesWithUpdatedTypes = edges.map((edge) => {
-    if (edge.sourceHandle) {
-      const edgeType = nodes.find((node) => node.type === "custom").data
-        .selects[edge.sourceHandle];
-      edge.type = edgeType;
-    }
-
-    return edge;
-  });
-
   return (
     <ReactFlow
       nodes={nodes}
-      edges={edgesWithUpdatedTypes}
+      edges={edges}
       onNodesChange={onNodesChange}
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
-      onInit={onInit}
       fitView
       attributionPosition="top-right"
     >
