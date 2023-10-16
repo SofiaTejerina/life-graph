56e8a235c1b9ca3a9a75b664562f809be800565a
Santiago Toscanini <github@account.toscanini.me>
2023-10-16 01:19:13 -0300

    Add cmd+s and fix nodemon bug

--- frontend/src/goal-dashboard/GoalSideBar.jsx
+++ frontend/src/goal-dashboard/GoalSideBar.jsx
@@ -4,38 +4,47 @@ import { InformationContext } from "../contexts/InformationContext";
 import axios from "axios";
 
 const goalInfo = (currentGoal) => {
-  return (
-    <>
-      <div>{currentGoal.title}</div>
-      <div>{currentGoal.data.progress}</div>
-      <div>{currentGoal.data.time}</div>
-      <div>{currentGoal.data.money}</div>
-    </>
-  );
+    return (
+        <>
+            <div>{currentGoal.title}</div>
+            <div>{currentGoal.data.progress}</div>
+            <div>{currentGoal.data.time}</div>
+            <div>{currentGoal.data.money}</div>
+        </>
+    );
 };
 
 const client = axios.create({
-  baseURL: "http://localhost:80/api/v1/graph",
+    baseURL: "http://localhost:80/api/v1/graph",
 });
 
 const onSave = (nodes, edges) => {
-  client.put("/edges", edges);
-  client.put("/nodes", nodes);
+    client.put("/edges", edges);
+    client.put("/nodes", nodes);
 };
 
 const GoalSideBar = () => {
-  const { currentGoal } = useContext(GoalContext);
-  const { nodes, edges } = useContext(InformationContext);
+    const { currentGoal } = useContext(GoalContext);
+    const { nodes, edges } = useContext(InformationContext);
 
-  return (
-    <div>
-      <button onClick={() => onSave(nodes, edges)}>Safe</button>
-      <br></br>
-      {currentGoal
-        ? goalInfo(currentGoal)
-        : "Click some node to read the information about"}
-    </div>
-  );
+    document.addEventListener('keydown', e => {
+        if ((e.ctrlKey || e.metaKey) && e.key === 's') {
+            // Prevent the Save dialog to open
+            e.preventDefault();
+
+            onSave(nodes, edges)
+        }
+    });
+
+    return (
+        <div>
+            <button onClick={() => onSave(nodes, edges)}>Safe</button>
+            <br></br>
+            {currentGoal
+                ? goalInfo(currentGoal)
+                : "Click some node to read the information about"}
+        </div>
+    );
 };
 
 export default GoalSideBar;
