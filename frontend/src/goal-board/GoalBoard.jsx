ef9ebea1d04c58f6fa9877972f85bc38db909fa1
Sofia Tejerina <sofiatejerina23@gmail.com>
2023-10-23 23:06:23 -0300

    No s euq ecambio aca

--- frontend/src/goal-board/GoalBoard.jsx
+++ frontend/src/goal-board/GoalBoard.jsx
@@ -1,5 +1,11 @@
 import React, { useCallback, useContext } from "react";
-import ReactFlow, { addEdge, Background, Controls, MiniMap, ReactFlowProvider, } from "reactflow";
+import ReactFlow, {
+  addEdge,
+  Background,
+  Controls,
+  MiniMap,
+  ReactFlowProvider,
+} from "reactflow";
 import "reactflow/dist/style.css";
 
 import "./baseNode.css";
@@ -9,40 +15,40 @@ import GroupNode from "./GroupNode";
 import { InformationContext } from "../contexts/InformationContext";
 
 const minimapStyle = {
-    height: 120,
+  height: 120,
 };
 
 const nodeTypes = {
-    simpleNode: SimpleNode,
-    groupNode: GroupNode,
+  simpleNode: SimpleNode,
+  groupNode: GroupNode,
 };
 
 const GoalBoard = () => {
-    const { nodes, onNodesChange, edges, setEdges, onEdgesChange } =
-        useContext(InformationContext);
+  const { nodes, onNodesChange, edges, setEdges, onEdgesChange } =
+    useContext(InformationContext);
 
-    const onConnect = useCallback((params) => {
-        setEdges((eds) => addEdge(params, eds)), [];
-    });
+  const onConnect = useCallback((params) => {
+    setEdges((eds) => addEdge(params, eds)), [];
+  });
 
-    return (
-        <ReactFlowProvider>
-            <ReactFlow
-                nodes={nodes}
-                edges={edges}
-                onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                onConnect={onConnect}
-                fitView
-                attributionPosition="top-right"
-                nodeTypes={nodeTypes}
-            >
-                <MiniMap style={minimapStyle} zoomable pannable/>
-                <Controls/>
-                <Background color="#aaa" gap={16}/>
-            </ReactFlow>
-        </ReactFlowProvider>
-    );
+  return (
+    <ReactFlowProvider>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+        attributionPosition="top-right"
+        nodeTypes={nodeTypes}
+      >
+        <MiniMap style={minimapStyle} zoomable pannable />
+        <Controls />
+        <Background color="#aaa" gap={16} />
+      </ReactFlow>
+    </ReactFlowProvider>
+  );
 };
 
 export default GoalBoard;
--- frontend/src/goal-dashboard/GoalSideBar.jsx
+++ frontend/src/goal-dashboard/GoalSideBar.jsx
@@ -5,47 +5,47 @@ import { GoalContext } from "../contexts/GoalGontext";
 import { InformationContext } from "../contexts/InformationContext";
 
 const client = axios.create({
-    baseURL: "http://localhost:80/api/v1/graph",
+  baseURL: "http://localhost:80/api/v1/graph",
 });
 
 const onSave = (nodes, edges) => {
-    client.put("/edges", edges);
-    client.put("/nodes", nodes);
+  client.put("/edges", edges);
+  client.put("/nodes", nodes);
 };
 
 const GoalSideBar = () => {
-    const { currentGoal } = useContext(GoalContext);
-    const { nodes, edges } = useContext(InformationContext);
+  const { currentGoal } = useContext(GoalContext);
+  const { nodes, edges } = useContext(InformationContext);
 
-    const goalInfo = () => {
-        return (
-            <>
-                <div>{currentGoal.title}</div>
-                <div>{currentGoal.data.progress}</div>
-                <div>{currentGoal.data.time}</div>
-                <div>{currentGoal.data.money}</div>
-            </>
-        );
-    };
-
-    document.addEventListener('keydown', e => {
-        if ((e.ctrlKey || e.metaKey) && e.key === 's') {
-            // Prevent the Save dialog to open
-            e.preventDefault();
-
-            onSave(nodes, edges)
-        }
-    });
-
+  const goalInfo = () => {
     return (
-        <div>
-            <button onClick={() => onSave(nodes, edges)}>Save</button>
-            <hr></hr>
-            {currentGoal
-                ? goalInfo()
-                : "Click some node to read the information about"}
-        </div>
+      <>
+        <div>{currentGoal.title}</div>
+        <div>{currentGoal.data.progress}</div>
+        <div>{currentGoal.data.time}</div>
+        <div>{currentGoal.data.money}</div>
+      </>
     );
+  };
+
+  document.addEventListener("keydown", (e) => {
+    if ((e.ctrlKey || e.metaKey) && e.key === "s") {
+      // Prevent the Save dialog to open
+      e.preventDefault();
+
+      onSave(nodes, edges);
+    }
+  });
+
+  return (
+    <div>
+      <button onClick={() => onSave(nodes, edges)}>Save</button>
+      <hr></hr>
+      {currentGoal
+        ? goalInfo()
+        : "Click some node to read the information about"}
+    </div>
+  );
 };
 
 export default GoalSideBar;
