0358e848dbcd574ec95da7fad28ea11c2c4b348a
Sofia Tejerina <sofiatejerina23@gmail.com>
2023-11-08 00:29:53 -0300

    Me olvide de commitear esto

---
+++ frontend/src/goal-board/NodeContextMenu.jsx
@@ -0,0 +1,23 @@
+import "./nodecontextMenu.css";
+
+const NodeContextMenu = ({
+  verticalPosition,
+  horizontalPosition,
+  onCreateGroupNodeClick,
+}) => {
+  return (
+    <div
+      className="external-container"
+      style={{
+        top: verticalPosition,
+        left: horizontalPosition,
+      }}
+    >
+      <p className="option-item box" onClick={onCreateGroupNodeClick}>
+        Agrupar nodos
+      </p>
+    </div>
+  );
+};
+
+export default NodeContextMenu;
